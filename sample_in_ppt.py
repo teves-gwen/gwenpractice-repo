310ba6204f163f39607c3682a8120da9090b2a84
teves-gwen <tevesgwenn.sch@gmail.com>
2025-03-14 23:10:45 +0800

    Add file.

---
+++ sample_in_ppt.py
@@ -0,0 +1,95 @@
+#print(type(True))
+#print(type(123_456_789))
+#print(type(1.43))
+#print(type("Hello!"))
+
+#print(123_456_789)
+
+#print("Helloe"[5])
+
+#name = "Steven" # 6 characters
+#name_length = len(name)
+#print(name_length) # 6 yung output
+
+#message = "Hello World!"
+#print(message) # Hello World!
+
+#message_me = "Snake case"
+
+"""
+This is a
+multi-line
+comment.
+"""
+
+"""myVariable = "Baso"
+message = f"Hello! My name is {myVariable}."
+print(message)
+
+name = "Plato"
+print(f"Hello! My name is {name}")
+"""
+"""
+age = input("Enter your age: ")
+print(f"Your age is {age}")
+
+age = input("Enter your age:")
+int_age = int(age)
+print(f"Your age after 3 years is {int_age + 3}")"""
+
+"""
+sum = 1 * 2
+print(sum)
+
+result = 4 ** 3
+print(result)
+
+res = 19 // 2
+print(res)
+
+"""
+"""
+a = 0
+a += 1
+# a = a + 1
+print(a)
+"""
+
+"""
+a = 0
+a = int(input("Enter a number:" ))
+if a > 7:
+    print("Edi true")
+else: 
+    print("Edi false")
+"""
+
+"""
+role = input("Enter your role: ")
+if role == "admin":
+    print("Hello Admin!")
+elif role == "reviewer":
+    print("Hello Reviewer!")
+else:
+    print("Hello User!")
+"""
+
+"""#TODO(member_1): Display choices.
+choice = input("Enter your choice: ")
+switch choice:
+	case "1":
+# TODO(member_2): Do item 1.
+case "2":
+# TODO(member_3): Do item 2."""
+
+
+
+
+
+
+
+
+
+
+
+
